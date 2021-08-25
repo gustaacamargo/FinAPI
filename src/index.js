5b2184a6045be8d8eafad2d536e85ee330f792e5
Gustavo Galdino de Camargo <camargo25.gustavo@gmail.com>
2021-08-25 19:49:25 -0300

    Adiciona rota de saque

--- src/index.js
+++ src/index.js
@@ -18,6 +18,19 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount;
+        }
+        else {
+            return acc - operation.amount;
+        }
+    }, 0)
+
+    return balance
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
@@ -52,6 +65,27 @@ app.post('/deposit', (request, response) => {
         amount,
         created_at: new Date(),
         type: 'credit'
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send();
+})
+
+app.post('/withdraw', (request, response) => {
+    const { customer } = request
+    const { amount } = request.body
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
     }
 
     customer.statement.push(statementOperation)
